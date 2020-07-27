7118545b7c6e1dea28d7dd34e7facda976d97b45
George Salamanca <GSalamanca@leggmason.com>
2020-07-26 20:48:45 -0400

    added patch route

--- routes/customers.js
+++ routes/customers.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb")
+
 module.exports = (app, customers) => {
     app.get('/test', async (req, res, next) => {
         try {
@@ -11,6 +13,17 @@ module.exports = (app, customers) => {
     app.post('/test', async (req, res, next) => {
         try {
             await customers.insertOne(req.body)
+            res.sendStatus(200)
+        } catch(e) {
+            next(e)
+        }
+    })
+
+    app.patch('/test', async (req, res, next) => {
+        const { _id: tempId, hello } = req.body
+        try {
+            const _id = ObjectId(tempId)
+            await customers.findOneAndUpdate({ _id }, { $set: { hello } })
             res.sendStatus(200)
         } catch(e) {
             next(e)
